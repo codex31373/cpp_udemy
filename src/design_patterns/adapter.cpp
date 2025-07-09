79c369b5dd54988cdcc512ec6f03dc169b970692
Mustafa Zaban <midnight90@mail.bg>
2025-07-09 21:57:41 +0300

    cmake refactor, adapter init

---
+++ src/design_patterns/adapter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(const int argc,const char *argv[])
+{
+
+    std::cout << "\033[92m" << "\nAdapter : 9 July 2025\n"
+              << "\033[0m" << std::endl;
+
+    return 0;
+}
