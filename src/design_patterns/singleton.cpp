0ce45308d6f1dce6a1b36b3996cb81aeb627aa40
Mustafa Zaban <midnight90@mail.bg>
2025-07-09 21:29:02 +0300

    singleton update

--- src/design_patterns/singleton.cpp
+++ src/design_patterns/singleton.cpp
@@ -15,19 +15,42 @@ struct SingletonTester
 };
 //----------------------
 
+
+class Logger {
+public:
+    virtual void log(const std::string& message) = 0;
+};
+
+class ConsoleLogger : public Logger {
+public:
+    void log(const std::string& message) override {
+        std::cout << message << std::endl;
+    }
+};
+
 class Singleton {
 public:
     static Singleton& getInstance() {
-        static Singleton instance; //"Meyers' Singleton" pattern, which is a modern and thread-safe way
+        static Singleton instance;  //"Meyers' Singleton" pattern, which is a modern and thread-safe way
         return instance;
     }
+
+    void setLogger(Logger* logger) {
+        this->logger = logger;
+    }
+
+    void doSomething() {
+        logger->log("Doing something...");
+    }
+
+private:
+    Logger* logger = nullptr;
+
+    Singleton() {} // Private constructor to prevent instantiation
 
     // Delete copy constructor and assignment operator to prevent copying
     Singleton(const Singleton&) = delete;
     Singleton& operator=(const Singleton&) = delete;
-
-private:
-    Singleton() {} // Private constructor to prevent instantiation
 };
 
 
@@ -37,6 +60,10 @@ int main(const int argc,const char *argv[])
     Singleton& singleton1 = Singleton::getInstance();
 
     std::cout << &singleton << " = " << &singleton1 << std::endl;
+
+    ConsoleLogger consoleLogger;
+    singleton.setLogger(&consoleLogger); //dependency injection with type of loger, can be defined another one
+    singleton.doSomething();
 
     std::cout << "\033[92m" << "\nSingleton : 9 July 2025\n"
               << "\033[0m" << std::endl;
