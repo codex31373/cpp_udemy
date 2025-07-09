322d48eb1a407930fcf4237ef9cefdf16ba9ab42
Mustafa Zaban <midnight90@mail.bg>
2025-07-09 16:47:52 +0300

    singleton : init

---
+++ src/design_patterns/singleton.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(const int argc,const char *argv[])
+{
+    std::cout << "\033[92m" << "\nSingleton : 9 July 2025\n"
+              << "\033[0m" << std::endl;
+
+    return 0;
+}
