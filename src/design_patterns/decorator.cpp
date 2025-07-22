67abba232d25b325e85243cf7f70c76e14d11dbf
Mustafa Zaban <midnight90@mail.bg>
2025-07-22 23:11:12 +0300

    decorator init

---
+++ src/design_patterns/decorator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(const int argc,const char *argv[])
+{
+    std::cout << "\033[92m" << "\nDecorator : 20 July 2025\n"
+              << "\033[0m" << std::endl;
+
+
+    return 0;
+}
