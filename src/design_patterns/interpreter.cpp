c5239482b824d87d45870d130c721e070da8fe97
Mustafa Zaban <midnight90@mail.bg>
2026-05-01 12:42:11 +0300

    interpreter init

---
+++ src/design_patterns/interpreter.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(const int argc,const char *argv[])
+{
+    std::cout << "\033[92m" << "\n Interpreter : 1 May 2026\n"
+              << "\033[0m" << std::endl;
+
+    return 0;
+}
