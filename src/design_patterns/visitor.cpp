41104e3760c0d4f2f08b57735bef4aef7f58ccbd
Mustafa Zaban <midnight90@mail.bg>
2026-05-10 21:17:26 +0300

    visitor init

---
+++ src/design_patterns/visitor.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(const int argc,const char* argv[])
+{
+    std::cout << "\033[92m" << "\nVisitor | 11 May 2026\n"
+              << "\033[0m" << std::endl;
+    return 0;
+}
